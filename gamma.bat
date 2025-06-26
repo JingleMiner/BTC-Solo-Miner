9d70d712dd1c61bda44bfe18adf9e23ab8819789
Steven <steven@jinglemining.com>
2025-06-26 11:00:52 +0800

    chore: add NerdAxeGamma configuration

---
+++ gamma.bat
@@ -0,0 +1,9 @@
+@echo off
+chcp 65001 >nul 2>&1
+setlocal enabledelayedexpansion
+
+set BOARD=NERDAXEGAMMA
+idf.py set-target esp32s3
+idf.py build
+esptool.py --chip esp32s3 merge_bin --flash_mode dio --flash_size 16MB --flash_freq 80m 0x0 build\bootloader\bootloader.bin 0x8000 build\partition_table\partition-table.bin 0x10000 build\esp-miner.bin 0x410000 build\www.bin 0xf10000 build\ota_data_initial.bin -o nerdaxegamma.bin
+bitaxetool --config config.cvs --firmware nerdaxegamma.bin
